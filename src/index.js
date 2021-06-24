c688078b8ed4eefd92e9786b03c136604b70880b
Juan Camilo <juancmz98@gmail.com>
2021-06-24 15:05:48 -0500

    React basic structure

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+;<React.StrictMode>
+  ReactDOM.render(
+  <App />, document.getElementById('app') )
+</React.StrictMode>
